d31700b59e24db3f8e91f840adce66ee4cadd99a
BoykoPetevBoev <boykopetevboev@gmail.com>
2020-06-11 16:47:43 +0300

    Express configuraion

---
+++ Workshop/config/express.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+
+module.exports = (app) => {
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.engine('.hbs', handlebars({
+        extname: '.hbs'
+    }));
+    app.set('view engine', '.hbs');
+
+    app.use('/static', express.static('static'));
+};
